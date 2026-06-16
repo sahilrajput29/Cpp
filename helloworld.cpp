a6305bd2d9dea817e683b26cf97c5bc5f0b879f0
Sahil kumar <sahilsingh1021y@gmail.com>
2026-06-16 20:45:38 +0530

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //This is a comment
+    /* This
+       is
+       a
+       multi-line
+       comment 
+    */
+    std::cout<<"I like Pizza!" <<std::endl; //endl means end line
+    std::cout<<"It's really good" <<std::endl;
+    cout<<"Today is Goodday" << '\n';
+    cout<<"Today is Sunday!!" <<endl; 
+    return 0;
+}
