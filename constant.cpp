0d0fe81398ab23eb89157aa625a8da5d9044dbef
Sahil kumar <sahilsingh1021y@gmail.com>
2026-06-18 22:02:51 +0530

    Add files via upload

---
+++ constant.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main(){
+    //The const keyword specifies that a variable's value is constant 
+    //tells the compiler to prevent anything to modifying it
+    //(read-only)
+
+    const double PI = 3.14159;
+    const int LIGHT_SPEED =299792458;
+    const int WIDTH = 1920;
+    const int HEIGHT = 1080;
+    
+    return 0;
+}
