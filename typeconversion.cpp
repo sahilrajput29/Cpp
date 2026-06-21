7ce0d0c5f49a367ab487b6ca19bc9c5c4cb75d47
Sahil kumar <sahilsingh1021y@gmail.com>
2026-06-21 10:25:40 +0530

    Add files via upload

---
+++ typeconversion.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+int main(){
+
+//type conversion = conversion a value of one data type to another
+//                  Implicit = Automatic
+//                  Explicit = Precedece value with new data type(int)
+
+    // double x = (int) 3.14;
+    // std::cout<<x;
+
+    // char x = 100;
+    // std::cout<<x;
+
+    int correct = 8;
+    int questions = 10;
+    double score = correct/(double)questions * 100;
+
+    std::cout<< score << "%";
+    return 0;
+}
