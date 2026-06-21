3bb18a3bb666d27d97d33b70e9848e1abac67913
Sahil kumar <sahilsingh1021y@gmail.com>
2026-06-21 10:25:22 +0530

    Add files via upload

---
+++ arithmeticoperator.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+int main(){
+    //arithmetic operators = returns the result of a specific 
+    //                       arithmetic operation(+ - * /)
+    //parenthesis
+    //multiplication & division
+    //addition & subtraction
+
+    //int students = 20;
+
+    //students = students +2;
+    //students+=1;
+    //students++;
+
+    //students = students - 2;
+    //students-=2;
+    //students--;
+
+    //students = students * 2;
+    //students*=2;
+
+    //students = students /2;
+    //students/=2;
+
+    //int remainder = students % 2;
+
+    int students = 6 - (5 + 4) * 3 / 2;
+
+
+    std::cout << students;
+
+    return 0;
+}
