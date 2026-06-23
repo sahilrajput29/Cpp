9af3ba0d090d81e169d1648df30d1bbff8a7aef5
Sahil kumar <sahilsingh1021y@gmail.com>
2026-06-23 22:28:53 +0530

    Add files via upload

---
+++ mathsfunction.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+int main(){
+
+    double x = 3.99;
+    double y = 4;
+    double z;
+
+    //z = std::max(x,y);
+    //z = std::min(x, y);
+    //z = pow(2,4);
+    //z = sqrt(9);
+    //z = abs(-3);
+    //z = round(x);
+    //z = ceil(x); //rounds a given floating-point number up to the nearest integer
+    //z = floor(x); //It is used to return the largest integral value n that is not greater than n.
+
+    std::cout<< z;
+
+    return 0;
+}
