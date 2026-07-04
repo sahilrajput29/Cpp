f788cb231d70e974c63665f6339f706093b7509d
Sahil kumar <sahilsingh1021y@gmail.com>
2026-07-04 23:52:58 +0530

    Add files via upload

---
+++ ternaryoperator.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+int main(){
+    //ternary operator ?: replacement to an if-else statement
+    //condition ? expression1 : expression2
+
+    // int grade = 50;     
+
+    // grade >= 60 ? std::cout<<"You passed!" : std::cout<<"You failed!";
+
+
+    // if(grade >= 60){
+    //     std::cout<<"You passed!";
+    // }
+    // else{
+    //     std::cout<<"You failed!";
+    // }
+
+    // int num = 9;
+    // num % 2 == 1? std::cout<<"Odd" : std::cout<<"Even";
+
+    // bool hungry = false;
+    // hungry ? std::cout<<"You are hungry!" : std::cout<<"You are not hungry!";
+    // std::cout<<(hungry ? "You are hungry!" : "You are not hungry!");
+
+    return 0;
+}
